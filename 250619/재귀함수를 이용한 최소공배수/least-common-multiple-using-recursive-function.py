2e6aada358049b3301eed263986af48a5c269c7b
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2025-06-19 01:10:23 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250619/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 250619/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -2,18 +2,21 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-def f(n):
-    global arr
-    if n == 0 :
-        return 1
-    else:
-        return f(n-1) * arr[n-1]
-        
-    if arr[n] % 2 == 0:
-        return arr[n]//2
-    elif arr[n] % 3 == 0:
-        return arr[n]//3
-    else : return arr[n]
+def gcd(a, b):
+    # 최대공약수 (GCD) 구하는 재귀함수
+    if b == 0:
+        return a
+    return gcd(b, a % b)
 
-    
-print(f(n-1))
+def lcm(a, b):
+    # 최소공배수 구하는 함수
+    return a * b // gcd(a, b)
+
+def lcm_list(arr, index=0):
+    # 재귀적으로 배열의 최소공배수를 구하는 함수
+    if index == len(arr) - 1:
+        return arr[index]
+    return lcm(arr[index], lcm_list(arr, index + 1))
+
+
+print(lcm_list(arr))
