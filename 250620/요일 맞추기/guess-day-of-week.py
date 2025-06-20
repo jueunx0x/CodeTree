4c38cb3f354d2787ae6c2da8e5f59c2b98c8676d
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2025-06-20 22:57:28 +0900

    Title: 요일 맞추기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250620/요일 맞추기/guess-day-of-week.py
+++ 250620/요일 맞추기/guess-day-of-week.py
@@ -5,29 +5,21 @@ m1, d1, m2, d2 = map(int, input().split())
 
 # 기준 연도 설정 (같은 해 기준이면 아무 연도나 OK)
 
+def num_of_days(m,d):
 # 각 달의 일수 (윤년은 고려하지 않음)
-day_list = [0, 31,28,31,30,31,30,31,31,30,31,30,31]
+    day_list = [0, 31,28,31,30,31,30,31,31,30,31,30,31]
+    total_days = 0
+    for i in range(1,m):
+        total_days+=day_list[i]
+    total_days+=d
 
+    return total_days
 # 요일 리스트 (정상 순서)
-weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-
-# 기준일: 1월 1일은 Monday → index = 1
-start_day_index = weekdays.index("Mon")
-
-# 기준일부터 목표일까지 며칠 차이 나는지 계산
-total_days = 0
+weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 # 출력
-if m1 == m2:
-    total_days = d2 - d1
-else:
-    # 시작 달의 남은 날짜
-    total_days += day_list[m1] - d1
-    # 중간 달들
-    for m in range(m1 + 1, m2):
-        total_days += day_list[m]
-    # 마지막 달의 날짜
-    total_days += d2
-
-target_day_index = (start_day_index + total_days) % 7
-print(weekdays[target_day_index])
+diff = 0 
+diff = num_of_days(m2,d2) - num_of_days(m1,d1) 
+while diff < 0 :
+    diff += 7
+print(weekdays[diff%7])
