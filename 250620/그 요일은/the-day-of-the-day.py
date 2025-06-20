8c8f5865ebb599befd270c33319e12f7f63290de
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2025-06-20 23:04:03 +0900

    Title: 그 요일은, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250620/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,18 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+# Please write your code here.
+
+def day_of_num(m,d):    
+    total_day = 0
+    days_list = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+    for i in range(1,m):
+        total_day += days_list[m]
+    total_day += d 
+    return total_day
+
+diff = 0
+weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+diff = day_of_num(m2,d2) - day_of_num(m1,d2)
+
+print(diff%7)
