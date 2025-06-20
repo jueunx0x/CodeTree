5c2b56683173db59282a62ffa300ebdbafa10832
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2025-06-20 18:38:08 +0900

    Title: 폭탄 해제, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250620/폭탄 해제/bomb-removal.py
@@ -0,0 +1,16 @@
+unlock_code, wire_color, seconds = input().split()
+seconds = int(seconds)
+
+# Please write your code here.
+
+class BombClear:
+    def __init__(self,code,color,sec):
+        self.code = code
+        self.color = color 
+        self.sec = sec
+
+firstBomb = BombClear(unlock_code,wire_color,34)
+
+print("code :",firstBomb.code)
+print("color :",firstBomb.color)
+print("second :",firstBomb.sec)
