d1482c08527c7e9093f8fa2e7ac3a7f40fe56ef9
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2025-07-15 22:01:57 +0900

    Title: 흰검 칠하기, Time: 82ms, Memory: 22MB, Status: Passed - Codetree

--- 250715/흰검 칠하기/painting-white-black.py
+++ 250715/흰검 칠하기/painting-white-black.py
@@ -1,41 +1,44 @@
-OFFSET = 1000
-SIZE = 4000  # 충분한 배열 크기 확보
-
+MAX_K = 100000
+# 변수 선언 및 입력:
 n = int(input())
-cur = 0
+a = [0] * (2 * MAX_K + 1)
+cnt_b = [0] * (2 * MAX_K + 1)
+cnt_w = [0] * (2 * MAX_K + 1)
+b, w, g = 0, 0, 0
 
-# 각 칸마다 white/black 횟수 기록
-white = [0] * SIZE
-black = [0] * SIZE
-last = [''] * SIZE  # 마지막 칠한 색
-
+cur = MAX_K
 for _ in range(n):
-    x, d = input().split()
+    x, c = tuple(input().split())
     x = int(x)
 
-    if d == 'L':
-        for i in range(cur - x, cur):
-            idx = i + OFFSET
-            white[idx] += 1
-            last[idx] = 'W'
-        cur -= x
-    else:  # R
-        for i in range(cur, cur + x):
-            idx = i + OFFSET
-            black[idx] += 1
-            last[idx] = 'B'
-        cur += x
+    if c == 'L':
+        # x칸 왼쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 1
+            cnt_w[cur] += 1
+            x -= 1
 
-# 결과 집계
-w_cnt, b_cnt, g_cnt = 0, 0, 0
+            if x: 
+                cur -= 1
+    else:
+        # x칸 오른쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 2
+            cnt_b[cur] += 1
+            x -= 1
 
-for i in range(SIZE):
-    if white[i] >= 2 and black[i] >= 2:
-        g_cnt += 1
-    elif white[i] or black[i]:
-        if last[i] == 'W':
-            w_cnt += 1
-        else:
-            b_cnt += 1
+            if x: 
+                cur += 1
 
-print(w_cnt, b_cnt, g_cnt)
+for i in range(2 * MAX_K + 1):
+    # 검은색과 흰색으로 두 번 이상 칠해진 타일은 회색입니다.
+    if cnt_b[i] >= 2 and cnt_w[i] >= 2: 
+        g += 1
+    # 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
+    elif a[i] == 1: 
+        w += 1
+    elif a[i] == 2: 
+        b += 1
+
+# 정답을 출력합니다.
+print(w, b, g)
