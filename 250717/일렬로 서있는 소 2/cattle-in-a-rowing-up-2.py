b5f8b947320e7b97425e49c6429b6c2e1ecc0066
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2025-07-17 23:47:25 +0900

    Title: 일렬로 서있는 소 2, Time: 58ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250717/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+A = list(map(int, input().split()))
+count = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if A[i] < A[j] < A[k]:
+                count += 1
+
+print(count)
