305ea9ae4f4126db5f2cce0f9e63501327b000c8
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2025-07-17 23:49:28 +0900

    Title: 일렬로 서있는 소 2, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 250717/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 250717/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,7 +5,7 @@ count = 0
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if A[i] < A[j] < A[k]:
+            if A[i] <= A[j] <= A[k] :
                 count += 1
 
 print(count)
